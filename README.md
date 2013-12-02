6501c78130c743d21daf88ec0f9d0661d539dada
Aziz Khoury <bentael@gmail.com>
2013-12-02 10:25:09 -0500

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-in dev mode
+### [UBB](http://www.ubbcentral.com/) Forum post-migration url redirector
+
+Quick and dirty solution to redirect my migrated [UBB](http://www.ubbcentral.com/) forum Topics/Forums/Users URLs to [NodeBB](http://nodebb.org/) ids.
+it can be used to anything other __target__ forum, doesn't have to be [NodeBB](http://nodebb.org/), but the __source__ forum must be [UBB](http://www.ubbcentral.com/).
+
+### Map
+
+You must create/generate a map of the __source__ topics/forums/users ids, each will map to either ids or slugs of the __target__ forum software you will be using.
+If you decide to migrate to NodeBB, use the migrator linked below, which will make that easy for you, otherwise, I'll let you figure that out,
+but your map must look like the [map.sample.json](map.sample.json) in structure. Look at the [source](server.js) if you're curious, it's not as bad as you think.
+
+### Install
+
+```bash
+# say your ubb forum is in /var/www/example.com/forums
+# cd $HOME
+npm install ubb-redirector
+cd ubb-redirector
+npm install
+
+# I would use 'forever' or 'supervisor' to keep it running
+sudo npm install -g forever
+
+forever -o out.log server.js --verbose --map /path/to/map.json --port 3000
+
+```
+
+### UBB Version
+    7.5.7
+
+### Migrator?
+
+if you want to migrate to [NodeBB](http://nodebb.org/) give this [nodebb-plugin-ubbmigrator](https://github.com/akhoury/nodebb-plugin-ubbmigrator) a shot
--- server.js
+++ server.js
@@ -1,75 +1,136 @@
-var http = require('http'),
-    argv = require('optimist').argv,
-    fse = require('fs-extra'),
-    path = require('path'),
-    mapFile = path.normalize(argv.m || argv.map ? argv.map : __dirname + '/map.sample.json'),
-    map, prelog = '[ubb-redirect] ',
+var express = require('express'),
+	argv = require('optimist').argv,
+	fse = require('fs-extra'),
+	_ = require('underscore'),
+	path = require('path'),
+	mapFile = path.normalize(argv.m || argv.map || __dirname + '/map.sample.json'),
+	Map,
 
-    ubbTopicPath = "ubbthreads.php/topics/",
-    ubbForumPath = "ubbthreads.php/forums/",
-    ubbUserPath = "ubbthreads.php/users/";
+// all these are preceeded by the Map.ubbRootPath value automatically
+	ubbTopicPath = "ubbthreads.php/topics/",
+	ubbForumPath = "ubbthreads.php/forums/",
+	ubbUserPath = "ubbthreads.php/users/",
+	ubbImagesPath = "images/",
 
-    if (fse.existsSync(mapFile)) {
-        console.log(prelog + 'reading map file: ' + mapFile + ' please be patient...');
-        map = fse.readJsonSync(mapFile);
-    } else {
-        throw new Error(prelog + '[Error] map file: ' + mapFile + ' does not exist.');
-    }
+	mapDefaults =
+	{
+		"ubbSiteRootDir": __dirname,
+		"ubbRootPath": "/forums",
+
+		"newSiteRootUrl": "http://example.com",
+		"newRootPath": "/forum",
+
+		"hard": {
+			"newPrefix": "",
+			"map": {
+				"ubb/login":"login",
+				"ubb/newuser":"register"
+			}
+		},
+
+		"forums": {
+			"newPrefix": "category/",
+			"map": {
+				"1": "1"
+			}
+		},
+
+		"users": {
+			"newPrefix": "user/",
+			"map": {
+				"1": "admin"
+			}
+		},
+
+		"topics": {
+			"newPrefix": "topic/",
+			"map": {
+				"1": "1"
+			}
+		}
+	};
+
+var log = argv.v || argv.verbose ? function(msg){ console.log('[ubb-redirect] ' + msg ); } : function(){};
+
+if (fse.existsSync(mapFile)) {
+	log('reading map file: ' + mapFile + ' please be patient...');
+	Map = _.extend(mapDefaults, fse.readJsonSync(mapFile) || {});
+} else {
+	var er = new Error('[Error] map file: ' + mapFile + ' does not exist.');
+	log(er);
+	throw er;
+}
 
 var getNewRoute = function(route) {
-    var newRoute = map.newRootPath;
+	var newRoute = Map.newRootPath + '/';
 
-    var topicIdx = route.indexOf(ubbTopicPath);
-    if (topicIdx >= 0) {
-        newRoute += appendToNewRoute(route, topicIdx, ubbTopicPath, "topics");
-        return newRoute;
-    }
+	var topicIdx = route.indexOf(ubbTopicPath);
+	if (topicIdx >= 0) {
+		log(route + ' looks like a ubb topic\'s page path');
+		newRoute += appendToNewRoute(route, topicIdx, ubbTopicPath, "topics");
+		return newRoute;
+	}
 
-    var forumIdx = route.indexOf(ubbForumPath);
-    if (forumIdx >= 0) {
-        newRoute += appendToNewRoute(route, forumIdx, ubbForumPath, "forums");
-        return newRoute;
-    }
+	var forumIdx = route.indexOf(ubbForumPath);
+	if (forumIdx >= 0) {
+		log(route + ' looks like a ubb forum\'s page path');
+		newRoute += appendToNewRoute(route, forumIdx, ubbForumPath, "forums");
+		return newRoute;
+	}
 
-    var userIdx = route.indexOf(ubbUserPath);
-    if (userIdx >= 0) {
-        newRoute += appendToNewRoute(route, userIdx, ubbUserPath, "users");
-        return newRoute;
-    }
+	var userIdx = route.indexOf(ubbUserPath);
+	if (userIdx >= 0) {
+		log(route + ' looks like a ubb user\'s profile page path');
+		newRoute += appendToNewRoute(route, userIdx, ubbUserPath, "users");
+		return newRoute;
+	}
 
-    // todo the hardcoded ones
+	var imageIdx = route.indexOf(ubbImagesPath);
+	if (imageIdx >= 0) {
+		log(route + ' looks like a ubb static path');
+		return '';
+	}
 
-    // todo the images ones
+	// no? check if any of the hardcoded ones
+	if(Object.keys(Map.hard.map).some(function(hardRoute){
+		if (route.indexOf(hardRoute) >= 0) {
+			log(route + ' looks like a hardcoded route.');
+			newRoute += Map.hard.newPrefix + Map.hard.map[hardRoute];
+			return true;
+		}
+	})) return newRoute;
 
-    return newRoute;
+	// anything else?
+
+	return newRoute;
 };
 
 var isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 var appendToNewRoute = function (route, idx, ubbPath, key){
-    var id = route.substr(idx + ubbPath.length);
-    var slash = id.indexOf('/');
-    if (slash >= 0) {
-       id = id.substring(0, slash)
-    }
-    id = parseInt(id, 10);
-    var newId = map[key].map[id]
-
-    if (isNumber(id) && newId) {
-        return map[key].newPrefix + newId;
-    } else {
-        return '';
-    }
+	var id = parseInt((route.substr(idx + ubbPath.length) || '').split('/')[0] || '', 10);
+	var newId = isNumber(id) ? Map[key].map[id] : '';
+	return newId ? Map[key].newPrefix + newId : '';
 };
 
-http.createServer(function (req, res) {
+var app = express();
 
-    var newRoute = getNewRoute(req.url);
-    console.log(prelog + req.url + ' --> ' + newRoute);
-    res.redirect('http://www.afraidtoask.com' + newRoute);
+// ubb static images
+var oldImagesDir = path.resolve(path.normalize(Map.ubbSiteRootDir + Map.ubbRootPath + path.sep + ubbImagesPath));
+var staticPath = Map.ubbRootPath + path.sep + ubbImagesPath;
+log(staticPath + ' will direct to your static ubbImages, they should live in: ' + oldImagesDir);
 
-}).listen(1337, '127.0.0.1');
+app.use(staticPath, express.static(oldImagesDir));
 
-console.log('Ubb Redirect running at http://127.0.0.1:1337/');
+app.get(Map.ubbRootPath + '*', function(req, res) {
+	var newRoute = getNewRoute(req.url);
+	log(req.url + ' --> ' + newRoute);
+	if (newRoute)
+		res.redirect(301, Map.newSiteRootUrl + newRoute);
+});
+
+var port = argv.p || argv.port || 3000;
+app.listen(port);
+log('running on localhost:' + port);
